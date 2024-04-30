28889f64c5f1b92c910b91e750788d14513695cc
bruno braga <brunobraga@protonmail.com>
2024-04-30 18:36:20 -0300

    Update custom plugins file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,71 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- This is how you map on normal mode
+-- vim.keymap.set('n', '<C-l><C-o>', ':terminal <Enter>', { noremap = true, silent = true })
 --
--- See the kickstart.nvim README for more information
-return {}
+
+vim.keymap.set('n', '<C-l><C-o>', ':FloatermNew nvim -c "DBUI" <Enter>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-e><C-w>', ':FloatermToggle <Enter>', { noremap = true, silent = true })
+
+return {
+  'voldikss/vim-floaterm',
+  {
+    -- Theme inspired by Atom
+    'Mofiqul/vscode.nvim',
+    config = function()
+      require('vscode').setup {
+        -- Set a style preset. 'dark' is default.
+        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+      }
+      require('vscode').load()
+    end,
+  },
+  {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow"
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+        "Bryley/neoai.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+        "NeoAI",
+        "NeoAIOpen",
+        "NeoAIClose",
+        "NeoAIToggle",
+        "NeoAIContext",
+        "NeoAIContextOpen",
+        "NeoAIContextClose",
+        "NeoAIInject",
+        "NeoAIInjectCode",
+        "NeoAIInjectContext",
+        "NeoAIInjectContextCode",
+    },
+    keys = {
+        { "<leader>as", desc = "summarize text" },
+        { "<leader>ag", desc = "generate git message" },
+    },
+    config = function()
+        require("neoai").setup({
+            -- Options go here
+        })
+    end,
+  }
+}
