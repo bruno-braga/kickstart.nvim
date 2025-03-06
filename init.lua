bdd786741a00f652cd4faf9a7da32012430bb75d
Bruno <brunobraga@protonmail.com>
2025-03-06 08:26:44 -0300

    Add ex command to save and run a test file

--- init.lua
+++ init.lua
@@ -203,6 +203,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_user_command('Wt', function(args)
+  vim.cmd(':w | !./vendor/bin/sail test ' .. vim.fn.expand '%')
+end, {})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -944,7 +948,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
