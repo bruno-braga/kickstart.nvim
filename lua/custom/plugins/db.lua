323a9f08f58f000e133cd99027b34f895a8ea4e7
Bruno <brunobraga@protonmail.com>
2025-03-06 08:27:00 -0300

    Add plugins

---
+++ lua/custom/plugins/db.lua
@@ -0,0 +1,22 @@
+return {
+  'tpope/vim-dadbod',
+  'kristijanhusak/vim-dadbod-ui',
+  'kristijanhusak/vim-dadbod-completion',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'postgresql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--
+
+vim.wo.relativenumber = true
+
+return {
+  'brett-griffin/PHPDocBlocks.vim',
+  {
+    'adalessa/laravel.nvim',
+    dependencies = {
+      'tpope/vim-dotenv',
+      'nvim-telescope/telescope.nvim',
+      'MunifTanjim/nui.nvim',
+      'kevinhwang91/promise-async',
+    },
+    cmd = { 'Laravel' },
+    keys = {
+      { '<leader>la', ':Laravel artisan<cr>' },
+      { '<leader>lr', ':Laravel routes<cr>' },
+      { '<leader>lm', ':Laravel related<cr>' },
+    },
+    event = { 'VeryLazy' },
+    opts = {},
+    config = true,
+  },
+}
